a20b97639826b16312de04ff043818457e566eb5
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-01-30 15:52:26 +0300

    feat: decided mission 5

---
+++ C#/семинар 1/задача 5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает последнюю 
+// цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите трехзначное число");
+
+string? numberStr = Console.ReadLine();
+
+if (numberStr != null) {
+    int number = Math.Abs(int.Parse(numberStr));
+    Console.WriteLine(number%10);
+}
