f7fc7a8d8447dae4f34473cad860f11703ed7aa1
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-01 00:32:38 +0300

    feat: decided exercise 8

---
+++ C#/домашняя работа 1/задача8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int N = 2;
+
+if (number < 2)
+{
+    Console.WriteLine("Введенное число меньше 2");
+}
+else
+{
+    while (N <= number)
+    {
+        Console.Write($"{N} ");
+        N += 2;
+    }
+}
+
