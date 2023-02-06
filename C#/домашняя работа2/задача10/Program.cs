633c12148aeb5f9523e3d4ea08543ad4dc0394f3
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-06 23:45:09 +0300

    feat: решил задачу 10

---
+++ C#/домашняя работа2/задача10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = numberRequest("введите число: ");
+
+int secondDigit = number / 10 % 10;
+
+Console.WriteLine(secondDigit);
