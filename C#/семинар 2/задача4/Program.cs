604cb5ea5ee07aa4332523c15afb9bd9b02a82d0
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-06 22:18:35 +0300

    feat: decided exercise 4

---
+++ C#/семинар 2/задача4/Program.cs
@@ -0,0 +1,23 @@
+﻿//напишите программу, которая принимает на вход 2 числа и проверяет, является ли одно из них квадратом другого
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int numberOne = numberRequest("введите число 1");
+int numberTwo = numberRequest("введите число 2");
+
+if (numberOne * numberOne == numberTwo)
+{
+    Console.WriteLine("второе число равно квадрату первого");
+}
+else if (numberTwo * numberTwo == numberOne)
+{
+    Console.WriteLine("первое число равное квадрату первого");
+}
+else
+Console.WriteLine("первое и второе число не являются квадратами друг друга");
+
