cb3776666d97dd9dfc2dedd51cb35ee6f7d7ac42
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-06 22:11:31 +0300

    feat: decided exercise 3

---
+++ C#/семинар 2/задача3/Program.cs
@@ -0,0 +1,21 @@
+﻿//напишите программу, которая принимает на вход число и проверяет кратно ли оно одновременно и 7 и 23.
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = numberRequest("введите число: ");
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("введенное число кратно 7 и 23");
+}
+else 
+Console.WriteLine($"введенное число не кратно одновременно 7 и 23");
+
+
+
+
