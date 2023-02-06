db9ff35787e721c704424fa77b4f3e40642c8d2f
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-06 21:56:18 +0300

    feat: decided exercise 2

---
+++ C#/семинар 2/задача2/Program.cs
@@ -0,0 +1,26 @@
+﻿// задача. 
+// напишите программу, которая принимает на вход два числа и определяет является
+// ли второе кратным первому. если второе число не кратно первому, то второе
+//число выводит остаток от деления
+
+// Console.WriteLine("Введите число 1");
+// Console.ReadLine();
+// Console.WriteLine("Введите число 1");
+// Console.ReadLine();    сделали через функцию
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int numberOne = numberRequest("введите число 1");
+int numberTwo = numberRequest("введите число 2");
+
+if (numberOne % numberTwo == 0)
+{
+    Console.WriteLine("второе число кратно первому");
+}
+else
+Console.WriteLine($"второе число не кратно первому. остаток от деления равен {numberOne % numberTwo}");
