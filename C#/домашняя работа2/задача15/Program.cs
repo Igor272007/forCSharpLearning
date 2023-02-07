144bcdb72e0d3803c3601d20db84584962b7b9aa
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-07 11:39:32 +0300

    feat: decided exercise 15

---
+++ C#/домашняя работа2/задача15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int numberRequest(string message)
+{
+     Console.WriteLine(message);
+     int number = Convert.ToInt32(Console.ReadLine());
+     return number;
+}
+
+int number = numberRequest("введите номер дня недели от 1 до 7: ");
+
+Console.WriteLine((number > 5 && number < 8) ? ("да") : ("нет"));
