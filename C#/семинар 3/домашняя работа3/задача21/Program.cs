70775fdb5ce7b629c632ec44b1b69b7ae8f2dc09
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-09 14:57:18 +0300

    decided exercise 21

---
+++ C#/семинар 3/домашняя работа3/задача21/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+//distanceBetweenpoint = sqrt((X2 - X1)^2 + (y2 - y1)^2 + (z2 - z1)^2)
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int x1 = GetNumber("введите координату Х первой точки");
+int y1 = GetNumber("введите координату Y первой точки");
+int z1 = GetNumber("введите координату Z первой точки");
+
+int x2 = GetNumber("введите координату Х второй точки");
+int y2 = GetNumber("введите координату Y второй точки");
+int z2 = GetNumber("введите координату Z второй точки");
+
+double GetDistance (int x1, int x2, int y1, int y2, int z1, int z2)//создаем функцию на поиск дистанции
+{
+    double result = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2) + Math.Pow(z2 - z1,2));//после запятой в скобках указана степень в которую возводим
+    return result;
+} 
+
+System.Console.WriteLine(GetDistance(x1,x2,y1,y2,z1,z2));
