90e18351c8ac51a00a7d083dff2f16d447457594
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-09 15:05:24 +0300

    decided exercise 23

---
+++ C#/семинар 3/домашняя работа3/задача23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result !=0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int numberN = GetNumber("введите число");
+
+for (int i = 1; i <= Math.Abs(numberN); i++)
+{
+    Console.Write($"{Math.Pow(i,3)} ");
+}
