b7c9b59876ff906c20e41d19009244fdb4e66e06
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-13 00:17:07 +0300

    feat: did not decided exercise 29

---
+++ C#/домашняя работа4/задача29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int number = 8;
+
+int[] InitArray(number)
+{
+    int[] arr = new int[number];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < number; i++)
+    {
+        arr[i] = rnd.Next();
+    }
+
+    return arr;
+}
+
+int[] arr = InitArray(4);
+
+Console.WriteLine(arr);
+
