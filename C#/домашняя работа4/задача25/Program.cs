12900ee2aa5fda469b3389bae28439cb2eef85dc
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-12 21:30:47 +0300

    feat: decided exercise 25

---
+++ C#/домашняя работа4/задача25/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int numberA = GetNumber("введите число А");
+int numberB = GetNumber("введите число B");
+
+int AToPowOfB = numberA;
+
+for (int i = 1; i < numberB; i++)
+{
+    AToPowOfB = AToPowOfB * AToPowOfB;
+}
+Console.WriteLine($"{numberA}, {numberB} -> {AToPowOfB}");
