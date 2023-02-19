4bb68e0191a54314ef61c27daabed8781ad7d76e
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-19 23:13:41 +0300

    feat: decided

---
+++ C#/домашняя работа6/задача41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"введите число");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"введите число");
+    }
+}
+
+int PositiveNumbersCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count++;
+    }
+    return count;
+}
+int length = Prompt("введите число М");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"количество положительных чисел = {PositiveNumbersCount(array)}");
