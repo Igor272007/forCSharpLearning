b2fdaa7f5513bc2209b62f3ea4cee9ad0bde9990
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-23 15:44:33 +0300

    feat: decided

---
+++ C#/семинар 7/задача2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] InitMatrix()
+{
+    int[,] matrix = new int[3,5];
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i + j;
+        }
+    }
+    return matrix;
+}
+void PritMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = InitMatrix();
+PritMatrix(matrix);
