0027feab3a039ae3c1e50c76d8e0614c9c719b23
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-23 18:38:20 +0300

    feat: decided not very bad

---
+++ C#/домашняя работа 7/задача 50/Program.cs
@@ -0,0 +1,92 @@
+﻿// Задача 50. Напишите программу, которая на вход 
+// принимает позиции элемента в двумерном массиве, и 
+// возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int number = GetNumber("введите число");
+
+int[,] InitMatrix()
+{
+    int[,] matrix = new int[3,4];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+
+int SearchNumber (int[,] matrix)
+{ 
+    int additionalVariable = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(number == matrix[i,j])
+            {
+                additionalVariable +=1;
+                
+            }
+        }
+    } 
+    return additionalVariable;
+}
+
+int additionalVariable = SearchNumber(matrix);
+
+if(additionalVariable > 0)
+{
+    Console.WriteLine($"число {number} есть в данном массиве");
+}
+else
+{
+    Console.WriteLine($"{number} такого числа в массиве нет");
+}
+
+
