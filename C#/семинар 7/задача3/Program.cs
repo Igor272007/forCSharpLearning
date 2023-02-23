ce915ecad3de7370463809b3f08a2a842483402f
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-23 16:06:19 +0300

    feat: decided

---
+++ C#/семинар 7/задача3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, и 
+// замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Илья Василевский: Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int[,] InitMatrix()
+{
+    int[,] matrix = new int[4,4];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(0,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SearchEvenNumber (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 != 0 && j % 2 != 0)
+            {
+               matrix[i,j] = matrix[i,j]* matrix[i,j];
+            }
+        }
+    }
+}
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+Console.WriteLine();
+SearchEvenNumber(matrix);
+PrintMatrix(matrix);
