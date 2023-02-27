332601ca24afa24f3678d4d24aac07b96ffc4e3e
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-27 19:39:28 +0300

    feat: not decided

---
+++ C#/домашняя работа 8/задача60/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив 
+// из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы 
+// каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] InitMatrix()
+{
+    int[,,] matrix = new int[2,2,2];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i,j,z] = rnd.Next(1,100);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                Console.Write($"{matrix[i,j,z]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+int[,,] matrix = InitMatrix();
+PrintMatrix(matrix);
+
+using System;
+using System.Collections.Generic;
+public class Example
+{
+    public static void FindIndex(int[,,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int z = 0; z < matrix.GetLength(2); z++)
+                {
+                    List<int> list = new List<int>(Console.Write($"{matrix[i,j,z]} "));
+                    Console.WriteLine();
+                    int index = list.IndexOf(matrix[i,j,z]);
+                    if (index != -10)
+                    {
+                        Console.WriteLine(String.Format($"{matrix[i,j,z]} {index(matrix[i,j,z])}"));
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+FindIndex(matrix);
