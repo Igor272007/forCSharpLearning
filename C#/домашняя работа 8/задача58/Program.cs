e881a2933279b86db4a1583d4b805ec7625a1c01
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-27 17:24:03 +0300

    feat: decided

---
+++ C#/домашняя работа 8/задача58/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет 
+// находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] InitMatrix()
+{
+    int[,] matrix = new int[2,2];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = InitMatrix();
+PrintMatrix(matrix);
+
+Console.WriteLine();
+
+int[,] InitNumbers()
+{
+    int[,] numbers = new int[2,2];
+    Random rnd = new Random();
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i,j] = rnd.Next(1,10);
+        }
+    }
+    return numbers;
+}
+
+void PrintNumbers(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write($"{numbers[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] numbers = InitNumbers();
+PrintNumbers(numbers);
+
+void FindNewArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = matrix[i,j] * numbers[i,j];
+        }
+    }
+    Console.WriteLine();
+    PrintMatrix(matrix);
+}
+
+FindNewArray(matrix);
