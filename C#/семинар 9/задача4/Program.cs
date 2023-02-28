3d7bf54df332d3d17f85f31e59e8257ff27a0fdb
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-28 23:20:43 +0300

    feat: decided

---
+++ C#/семинар 9/задача4/Program.cs
@@ -0,0 +1,41 @@
+﻿// задача 69. Напишите программу, которая
+// на вход принимает два числа А и В, возводит 
+// число А в степень В с помощью рекурсии.
+// А = 3; В = 5 - > 243(3)
+// A = 2; B = 3 -> 8(2)
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result !=0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int GetPowNumbersRec(int numberA, int numberB)
+{
+    if (numberB == 1)
+    return numberA;
+    else
+    {
+        return numberA * GetPowNumbersRec(numberA, numberB - 1);
+    }
+}
+
+int numberA = GetNumber("введите число: ");
+int numberB = GetNumber("введите число: ");
+
+Console.WriteLine(GetPowNumbersRec(numberA, numberB));
