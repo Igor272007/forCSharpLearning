06cb2250c1578d00583d2d286229a64506975f5d
Igor Kuznetsov <kuznetcov28@mail.ru>
2023-02-28 22:59:03 +0300

    feat: decided

---
+++ C#/семинар 9/задача3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 67: Напишите программу, 
+// которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result !=0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int GetNaturalNumbersRec(int number, int sumOfDigit)
+{
+    if (number == 0)
+    return sumOfDigit;
+    else
+    {
+        sumOfDigit += number % 10;
+        number /= 10;
+        return GetNaturalNumbersRec(number, sumOfDigit);
+    }
+}
+int number = GetNumber("введите число: ");
+int sumOfDigit = 0;
+Console.WriteLine(GetNaturalNumbersRec(number, sumOfDigit));
